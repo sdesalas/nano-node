984420b70b86fb666d95780c1b1eae8d3e350285
Steven de Salas <steven@desalasworks.com>
2021-06-20 10:34:25 +0200

    Review docker build

--- docker/ci/build.sh
+++ docker/ci/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 REPO_ROOT=`git rev-parse --show-toplevel`
 pushd $REPO_ROOT
-docker build -f docker/ci/Dockerfile -t nano-ci:latest .
+docker build -f docker/ci/Dockerfile-base -t nano-ci:base .
+echo "Finished BASE, now building GCC"
+sleep 5
+docker build -f docker/ci/Dockerfile-gcc -t nano-ci:gcc .
 popd
